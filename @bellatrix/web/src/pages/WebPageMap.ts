5d6dabeb3f12b6976e961f643974158e25fecf9f
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2024-04-07 22:40:20 +0300

    Update WebPageMap.ts

--- @bellatrix/web/src/pages/WebPageMap.ts
+++ @bellatrix/web/src/pages/WebPageMap.ts
@@ -1,8 +1,11 @@
-import { ServiceLocator } from "@bellatrix/core/utilities";
+import { Ctor } from "@bellatrix/core/types";
 import { ComponentService } from "@bellatrix/web/services";
+import { WebComponent } from "@bellatrix/web/components";
+import { ServiceLocator } from "@bellatrix/core/utilities";
+import { BrowserAutomationTool } from "infrastructure/browserautomationtools/core";
 
 export abstract class WebPageMap {
-    protected get components(): ComponentService {
-        return ServiceLocator.resolve(ComponentService);
+    create<T extends WebComponent>(type: Ctor<T, ConstructorParameters<typeof WebComponent>>) {
+        return new ComponentService(ServiceLocator.resolve(BrowserAutomationTool), type);
     }
 }
