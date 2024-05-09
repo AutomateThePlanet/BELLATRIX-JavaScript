94d7bbdb8522929c2c99359b9431fffd3bc84ddf
Teodor Nikolov <teo8447@gmail.com>
2024-05-09 15:33:38 +0300

    constants naming convention

--- @bellatrix/core/src/constants/Symbols.ts
+++ @bellatrix/core/src/constants/Symbols.ts
@@ -1,17 +1,17 @@
-const beforeAll = Symbol('bellatrix:beforeAll');
-const beforeEach = Symbol('bellatrix:beforeEach');
-const afterEach = Symbol('bellatrix:afterEach');
-const afterAll = Symbol('bellatrix:afterAll');
-const test = Symbol('bellatrix:test');
-const suite = Symbol('bellatrix:suite');
-const currentTest = Symbol('bellatrix:currentTest');
+const BEFORE_ALL = Symbol('bellatrix:beforeAll');
+const BEFORE_EACH = Symbol('bellatrix:beforeEach');
+const AFTER_EACH = Symbol('bellatrix:afterEach');
+const AFTER_ALL = Symbol('bellatrix:afterAll');
+const TEST = Symbol('bellatrix:test');
+const SUITE = Symbol('bellatrix:suite');
+const CURRENT_TEST = Symbol('bellatrix:currentTest');
 
 export const Symbols = {
-    beforeAll,
-    beforeEach,
-    afterEach,
-    afterAll,
-    test,
-    suite,
-    currentTest,
+    BEFORE_ALL,
+    BEFORE_EACH,
+    AFTER_EACH,
+    AFTER_ALL,
+    TEST,
+    SUITE,
+    CURRENT_TEST,
 } as const;
