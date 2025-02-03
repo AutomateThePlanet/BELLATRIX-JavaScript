c07ef98d47cefefab084f11662cb390114208522
Teodor Nikolov <teo8447@gmail.com>
2025-02-03 19:42:41 +0200

    added category in CI test (still order dependent)

--- example/tests/ProductPurchaseTests.test.ts
+++ example/tests/ProductPurchaseTests.test.ts
@@ -1,4 +1,5 @@
 import { Test, TestClass } from '@bellatrix/web/test';
+import { Category } from '@bellatrix/core/decorators';
 import { WebTest } from '@bellatrix/web/infrastructure';
 import { Button } from '@bellatrix/web/components';
 import { ExtraWebHooks } from '@bellatrix/extras/hooks';
@@ -9,6 +10,7 @@ import { WebServiceHooks } from '@bellatrix/web/services/utilities';
 import { NavigationService } from '@bellatrix/web/services';
 
 @TestClass
+@Category('CI') // TODO: make it order independent
 export class ProductPurchaseTests extends WebTest {
     override async configure(): Promise<void> {
         await super.configure();
