8c18b44795faa60ff371b5c387de9e5c4be43729
Teodor Nikolov <teo8447@gmail.com>
2025-02-04 01:14:55 +0200

    limit debug to methods

--- @bellatrix/core/src/decorators/Debug.ts
+++ @bellatrix/core/src/decorators/Debug.ts
@@ -1,22 +1,8 @@
-import { Symbols } from '@bellatrix/core/constants';
 import { BellatrixTest } from '@bellatrix/core/infrastructure';
 import { getTestMetadata } from '@bellatrix/core/test/props';
 import { ParameterlessCtor } from '@bellatrix/core/types';
 
-export function Debug<T extends BellatrixTest>(target: T, methodName: string): void;
-export function Debug<T extends BellatrixTest>(target: ParameterlessCtor<T>): void;
-export function Debug<T extends BellatrixTest>(target: T | ParameterlessCtor<T>, methodName?: string): void {
-    if (typeof target === 'function') {
-        const testClass = target.prototype;
-        const testMethods = Object.getOwnPropertyNames(testClass).filter(method => typeof testClass[method] === 'function' && Reflect.hasMetadata(Symbols.TEST, testClass[method]));
-
-        for (const testMethod of testMethods) {
-            const metadata = getTestMetadata(testClass[testMethod], testClass);
-            metadata.only = true;
-        }
-    } else {
-        const metadata = getTestMetadata(target[methodName as keyof T] as (...args: unknown[]) => (Promise<void> | void), target.constructor as ParameterlessCtor<T>);
-        metadata.only = true;
-        console.log(metadata);
-    }
+export function Debug<T extends BellatrixTest>(target: T, methodName?: string): void {
+    const metadata = getTestMetadata(target[methodName as keyof T] as (...args: unknown[]) => (Promise<void> | void), target.constructor as ParameterlessCtor<T>);
+    metadata.only = true;
 }
